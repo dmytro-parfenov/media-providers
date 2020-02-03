4c990e136d108e199a556e87996d51c6f85b7b1f
Dmitry Parfenov <dmitryparfenov937@gmail.com>
2020-02-03 20:56:44 +0200

    feat: rename project

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/media-finder'),
+      dir: require('path').join(__dirname, './coverage/mif-projects-frontend-candidate-test'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },

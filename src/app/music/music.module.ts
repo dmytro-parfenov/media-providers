7a005176fa8875ae04df891d7007de7b706e0b24
Dmitry Parfenov <dmitryparfenov937@gmail.com>
2020-02-03 18:52:34 +0200

    feat: prepare modules

---
+++ src/app/music/music.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MusicComponent } from './music/music.component';
+import {MusicRoutingModule} from './music-routing.module';
+
+
+
+@NgModule({
+  declarations: [MusicComponent],
+  imports: [
+    CommonModule,
+    MusicRoutingModule
+  ]
+})
+export class MusicModule { }
---
+++ src/app/music/music/music.component.ts
@@ -0,0 +1,16 @@
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-music',
+  templateUrl: './music.component.html',
+  styleUrls: ['./music.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MusicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
